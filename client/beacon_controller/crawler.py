a1acb3339d60336e0ce95d9a72347bda49000a5c
Lance Hannestad <lhannest@sfu.ca>
2018-08-03 21:24:41 -0700

    Added crawler.py

---
+++ client/beacon_controller/crawler.py
@@ -0,0 +1,25 @@
+import requests
+
+from typing import Dict, List
+from functools import lru_cache
+
+_uri_pattern = 'http://biothings.io/explorer/api/v2/crawler?input_type={prefix}&input_value={local_id}'
+
+@lru_cache(maxsize=128)
+def crawl(curie:str) -> Dict[str, List[dict]]:
+    """
+    Retreives a dictionary mapping categories onto a list of associations that
+    involve the given CURIE identifier.
+
+    :param curie: a CURIE identifier of concept of interest
+    """
+    prefix, local_id = curie.split(':', 1)
+    uri = _uri_pattern.format(prefix=prefix.lower(), local_id=local_id)
+
+    response = requests.get(uri)
+
+    if response.ok:
+        data = response.json()
+        return data['linkedData']
+    else:
+        return {}
