ea25cc792e2d35eebb40a5436333d98875a7d789
Lance Hannestad <lhannest@sfu.ca>
2018-08-27 14:35:11 -0700

    Removed reference to update_controller.py

--- client/beacon_controller/__init__.py
+++ client/beacon_controller/__init__.py
@@ -2,4 +2,3 @@ from . import utils
 from .concepts_controller import get_concept_details, get_concepts, get_exact_matches_to_concept_list
 from .statements_controller import get_statement_details, get_statements
 from .metadata_controller import get_concept_categories, get_knowledge_map, get_predicates
-from .update_controller import update
